d9c37579deeaf93d6728ca4ba3d26afc2c9f8a07
kshitija <kkarkar@mail.csuchico.edu>
2017-03-28 22:40:26 -0700

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿create table Pic(
+	id int not null,
+	Name varchar(MAX),
+	Descrip varchar(MAX),
+	Image1 varchar(50)
+)
---
+++ dbo.PicStore.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Description] VARCHAR(50) NULL, 
+    [Image] VARCHAR(MAX) NULL
+)
